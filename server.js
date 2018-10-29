71747ad1a95c0519003140d185fd82c02efb3b73
Tom Dennis <tdennis913@gmail.com>
2018-10-29 17:53:29 -0400

    initial mern deploy

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/nyt-search-react-mongo',
+    {
+        useMongoClient: true
+    }
+);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`starting server at PORT: ${PORT}`));
